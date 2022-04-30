922d786d29202005fa9f2e851e6665e23634e3a4
Esneider Ospina <sneuder8@gmail.com>
2022-04-30 10:33:40 -0500

    HTML plugin configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -6,5 +7,11 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
-  }
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      filename: 'index.html',
+      template: './public/index.html'
+    })
+  ]
 }
